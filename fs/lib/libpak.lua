bd3abe2e3f908d6fe4f2ffc8655bcb7871dbae45
tete <tetex7@outlook.com>
2024-02-03 16:09:41 -0500

    added things

--- fs/lib/libpak.lua
+++ fs/lib/libpak.lua
@@ -1,5 +1,15 @@
 libpak = {}
 
+
+---comment
+---@param name string
+---@param from string
+---@param to string
+local function nl(name, from, to)
+end
+---@type function
+libpak.prer = nl
+
 libpak.name = ""
 function libpak.pak()
     fs.delet(PWD.."/"..libpak.name)
